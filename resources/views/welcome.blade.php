83654bb20808c3f071985fbadab447aedb9afd7c
salawekesa~ <wekesa9235@gmail.com>
2022-11-17 18:09:23 +0300

    feat-routes and views 3 pages linked

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ url('/styles/style.css') }}">
+    <title>Sprint</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contacts</li>
+
+            </ul>
+        </header>
+        <p>This is the landing page</p>
+    </div>
+
+</body>
+</html>
